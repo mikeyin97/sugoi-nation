bf0a0e720ac8f58c1c2e95b8d7dfb4b9e6b70240
mikeyin97 <yinjitong@hotmail.com>
2016-11-19 16:15:41 -0500

    adding the neural network attempt

--- other/newnn.js
+++ other/newnn.js
@@ -1,16 +1,54 @@
-var url = 'http://graph.facebook.com/517267866/?fields=picture';
+var http = require('http'); //the variable doesn't necessarily have to be named http
+http.createServer(function(req, res) {});
+var url = 'http://localhost:8000/';
+var myNet = new Architect.Perceptron(5, 3, 1);
+var trainingSet = [];
 
 http.get(url, function(res){
     var body = '';
 
-    res.on('data', function(chunk){
-        body += chunk;
-    });
+    //res.on('data', function(chunk){
+    //    body += chunk;
+    //});
 
     res.on('end', function(){
-        var fbResponse = JSON.parse(body);
-        console.log("Got a response: ", fbResponse.picture);
+        var data = JSON.parse(body);
+        console.log("Got a response");
+        var inputvec = [];
+               if data.format = "desktop"{
+                    inputvec.push(0,0,1);
+                }
+                else if data.format = "app"{
+                    inputvec.push(0,1,0);
+                }
+                else {
+                  inputvec.push(0,0,1);
+                  } 
+                
+                if data.platform = "banner"{
+                    inputvec.push (1);
+                }
+                else{
+                    inputvec.push (0);
+                }
+                
+                inputvec.push(((data.timestamp)/60/60/24)%24);
+                
+                var outputvec = [];
+                outputvec.push(1/(1+exp(-data.impressions/2000)));
+
+                trainingSet.push({input = inputvec, output = outputvec});
+                console.log("complete")
     });
 }).on('error', function(e){
       console.log("Got an error: ", e);
 });
+
+var trainingOptions = {
+rate: .1,
+iterations: 20000,
+error: .005,
+}
+
+myNet.trainer.train(trainingSet, trainingOptions);
+console.log(myNet);
