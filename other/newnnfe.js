08a3ac341cb547e89783812559ab58e02930b2c2
mikeyin97 <yinjitong@hotmail.com>
2016-11-20 03:20:00 -0500

    done neural network, testing

--- other/newnnfe.js
+++ other/newnnfe.js
@@ -18,7 +18,7 @@ var trainingOptions = {
 var file = 'C:\Users\\Michael\\Documents\\GitHub\\sugoi-nation\\other\\test2.json'
 
 var url = 'http://localhost:8000/impressions?dc=NA';
-
+/*
 var trainingSet = [
   {
     input: [0,0,1,0.12,0,0,0,0,1,1],
@@ -41,10 +41,10 @@ var trainingOptions = {
 }
 
 myNet.trainer.train(trainingSet, trainingOptions);
-var exported = myNet.toJSON();
-        jsonfile.writeFile(file, exported, function (err) {
-            console.error(err)
-        })
+console.log(myNet);
+      
+*/
+
 
 http.get(url, function (res) {
     var body = '';
@@ -61,7 +61,7 @@ http.get(url, function (res) {
             var inputvec = [];
             var outputvec = [];
             if (data.data[i].format == "desktop") {
-                inputvec.push(0, 0, 1);
+                inputvec.push(1, 0, 0);
             }
             else if (data.data[i].format == "app") {
                 inputvec.push(0, 1, 0);
@@ -94,10 +94,11 @@ http.get(url, function (res) {
         console.log(myNet);
         impressions = -5000*Math.log(1/(myNet.activate([0,0,1,1,8]))-1);
         console.log(impressions);
-        var exported = myNet.toJSON();
-        jsonfile.writeFile(file, exported, function (err) {
-            console.error(err)
-        })
+        //var exported = myNet.toJSON();
+        //jsonfile.writeFile(file, exported, function (err) {
+        //    console.error(err)
+        //})
+        console.log(myNet.activate.toString());
     });
 }).on('error', function (e) {
     console.log("Got an error: ", e);
