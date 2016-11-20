13d9b84f183625416ab29d4c22f36e74b437f7bb
mikeyin97 <yinjitong@hotmail.com>
2016-11-20 10:41:36 -0500

    completed webpage, or so they say

--- other/newnnfe.js
+++ other/newnnfe.js
@@ -45,7 +45,6 @@ console.log(myNet);
       
 */
 
-
 http.get(url, function (res) {
     var body = '';
 
@@ -60,26 +59,28 @@ http.get(url, function (res) {
         for (var i in data.data) {
             var inputvec = [];
             var outputvec = [];
-            if (data.data[i].format == "desktop") {
+            //console.log(data.data[i].platform);
+            if (data.data[i].platform == "desktop") {
                 inputvec.push(1, 0, 0);
             }
-            else if (data.data[i].format == "app") {
+            else if (data.data[i].platform == "app") {
                 inputvec.push(0, 1, 0);
             }
             else {
                 inputvec.push(0, 0, 1);
             }
 
-            if (data.data[i].platform == "video") {
+            if (data.data[i].format == "video") {
                 inputvec.push(1);
             }
             else {
                 inputvec.push(0);
             }
-
+                
             inputvec.push(((parseFloat(data.data[i].timestamp) / 60 / 60 / 24) % 24)/24);
             //console.log(((parseFloat(data.data[i].timestamp) / 60 / 60 / 24) % 24)/24);
-            outputvec.push(1 / (1 + Math.exp(-data.data[i].impressions / 5000)));
+            outputvec.push(1 / (1 + Math.exp(-(data.data[i].spend-70000) / 50000)));
+            console.log(1 / (1 + Math.exp(-(data.data[i].spend-70000) / 50000)));
             //console.log(1 / (1 + Math.exp(-data.data[i].impressions / 5000)));
             //console.log(inputvec);
             trainingSet.push({input: inputvec,output: outputvec});
@@ -89,7 +90,8 @@ http.get(url, function (res) {
         
         // console.log("TRAINING!");
         // 
-        console.log("training!")
+        console.log("training!");
+        console.log(trainingSet);
         myNet.trainer.train(trainingSet, trainingOptions);
         console.log(myNet);
         impressions = -5000*Math.log(1/(myNet.activate([0,0,1,1,8]))-1);
